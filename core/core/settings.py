115820c1d365338796d2933e55405da07a6e7708
masoud533 <mapar10278@gmail.com>
2026-02-05 17:51:51 +0330

    setup static and media

--- core/core/settings.py
+++ core/core/settings.py
@@ -117,7 +117,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = '/media'
+MEDIA_ROOT = BASE_DIR / "media"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "staticfiles",
+]
 
 
 # user manager config
