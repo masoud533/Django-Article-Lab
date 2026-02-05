e0cd53646ec4057db8ed37361475ab93d7a8107a
masoud533 <mapar10278@gmail.com>
2026-02-05 17:36:18 +0330

    add user profile to admin

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, Profile
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 
@@ -41,3 +41,4 @@ class CustomUserAdmin(UserAdmin):
     )
 
 admin.site.register(User,CustomUserAdmin)
+admin.site.register(Profile)
