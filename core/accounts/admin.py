9e7a22c9a0afa50e1ae7ab5e5d43d276f5e4562f
masoud533 <mapar10278@gmail.com>
2026-02-05 11:01:08 +0330

    modified accounts admin

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
-# Register your models here.
+class CustomUserCreationForm(CustomUserCreationForm):
+
+    class Meta:
+        model = User
+        fields = ('email',)
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    add_form = CustomUserCreationForm
+    list_display = ('email', "is_superuser", "is_active")
+    list_filter = ('email', "is_superuser", "is_active")
+    search_fields = ('email')
+    ordering = ('email')
+    fieldsets = (
+        (None, {
+            "fields": (
+                'email', 'password'
+            ),
+        }),
+        ('permissions', {
+            "fields": (
+                "is_staff", 'is_active', 'is_superuser'
+            ),
+        }),
+        ('group permissions', {
+            "fields": (
+                "groups", 'user_permissions',
+            ),
+        }),
+        ('important date', {
+            "fields": (
+                "last_login",
+            ),
+        }),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password', 'is_staff', 'i_superuser', 'is_active')
+        }),
+    )
+
+admin.site.register(User,CustomUserAdmin)
