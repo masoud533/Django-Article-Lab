b40d1c7dd78cd87ffccc1de26254a3fcf190c275
masoud533 <mapar10278@gmail.com>
2026-02-09 18:51:42 +0330

    add post detail url

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('fbv-index', views.indexView, name="fbv-test"),
     path('cbv-index', views.IndexView.as_view(), name="cbv-test"),
     path('post/', views.PostList.as_view(), name='post-list'),
+    path('post/<int:pk>/', views.PostDetail.as_view(), name='post-detail'),
     path('go-to-index', RedirectView.as_view(pattern_name="blog:cbv-test"), name="redirect-to-index")
 ]
