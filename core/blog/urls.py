d5fb74b437a1777be4088240b2d6d98cb84c4cb6
masoud533 <mapar10278@gmail.com>
2026-02-10 11:56:22 +0330

    add update view url

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('post/', views.PostList.as_view(), name='post-list'),
     path('post/<int:pk>/', views.PostDetail.as_view(), name='post-detail'),
     path('post/create/', views.ContactFormView.as_view(), name='post-create'),
+    path('post/<int:pk>/edit/', views.UpdatePostView.as_view(), name='post-edit'),
     path('go-to-index', RedirectView.as_view(pattern_name="blog:cbv-test"), name="redirect-to-index")
 ]
