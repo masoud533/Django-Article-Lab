b9f2aac26070f535624d77869c6690ec8a650671
masoud533 <mapar10278@gmail.com>
2026-02-21 02:23:57 +0330

    modified blog urls.pu

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,16 +1,19 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 from django.views.generic.base import RedirectView
 
 app_name = "blog"
 
 urlpatterns = [
-    path('fbv-index', views.indexView, name="fbv-test"),
-    path('cbv-index', views.IndexView.as_view(), name="cbv-test"),
+    # path('fbv-index', views.indexView, name="fbv-test"),
+    # path('cbv-index', views.IndexView.as_view(), name="cbv-test"),
     path('post/', views.PostList.as_view(), name='post-list'),
     path('post/<int:pk>/', views.PostDetail.as_view(), name='post-detail'),
     path('post/create/', views.ContactFormView.as_view(), name='post-create'),
     path('post/<int:pk>/edit/', views.UpdatePostView.as_view(), name='post-edit'),
     path('post/<int:pk>/delete/', views.DeletePostView.as_view(), name='post-delete'),
-    path('go-to-index', RedirectView.as_view(pattern_name="blog:cbv-test"), name="redirect-to-index")
+    path('go-to-index', RedirectView.as_view(pattern_name="blog:cbv-test"), name="redirect-to-index"),
+
+    path('api/v1/',include('blog.api.v1.urls')),
+    
 ]
