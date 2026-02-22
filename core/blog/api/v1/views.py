47b511285bdd39a773111195fae45e68ced04962
masoud533 <mapar10278@gmail.com>
2026-02-22 12:11:24 +0330

    modified post list api

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -7,12 +7,14 @@ from django.shortcuts import get_object_or_404
 
 @api_view()
 def postlist(request):
-    return Response('ok')
+    posts = Post.objects.filter(status=True)
+    serializer = postSerializer(posts,many=True)
+    return Response(serializer.data)
 
 @api_view()
 def postDetail(request, id):
 
-    post = get_object_or_404(Post,pk=id)
+    post = get_object_or_404(Post,pk=id,status=True)
 
     serializer = postSerializer(post)
     return Response(serializer.data)
