f68b82c871bd44124bc7f90ea05b6a8584589938
masoud533 <mapar10278@gmail.com>
2026-02-22 12:03:21 +0330

    modified post Detail view

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -2,6 +2,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import postSerializer
 from blog.models import Post
+from django.shortcuts import get_object_or_404
+
 
 @api_view()
 def postlist(request):
@@ -10,7 +12,7 @@ def postlist(request):
 @api_view()
 def postDetail(request, id):
 
-    post = Post.objects.get(pk=id)
+    post = get_object_or_404(Post,pk=id)
 
     serializer = postSerializer(post)
     return Response(serializer.data)
