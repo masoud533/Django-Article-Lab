377454b57083003574884377a37e8e7dcae79d57
masoud533 <mapar10278@gmail.com>
2026-02-23 13:19:00 +0330

    modified project

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,5 +1,5 @@
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.response import Response
 from .serializers import postSerializer
 from blog.models import Post
@@ -7,7 +7,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework import status
 
 @api_view(["GET", "POST"])
-@permission_classes([IsAuthenticatedOrReadOnly])
+@permission_classes([IsAuthenticated])
 def postlist(request):
     if request.method == "GET":
         posts = Post.objects.filter(status=True)
