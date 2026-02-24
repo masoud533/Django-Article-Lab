6105b1db48d699f883cbb5bee15634e99e31f970
masoud533 <mapar10278@gmail.com>
2026-02-24 14:32:32 +0330

    add PostViewSet

--- core/blog/api/v1/urls.py
+++ core/blog/api/v1/urls.py
@@ -11,4 +11,7 @@ urlpatterns = [
 
     path('post/', views.PostList.as_view(), name='post-list'),
     path('post/<int:pk>/', views.PostDetail.as_view(), name='post-detail'), 
+
+    path('post2/', views.PostViewSet.as_view({'get':'list', 'post':'create'}), name='post-list2'),
+    path('post2/<int:pk>/', views.PostViewSet.as_view({'get':'retrieve', 'put':'update', 'patch':'partial_update','delete':'destroy'}), name='post-detail2'),
 ]
--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from .serializers import PostSerializer
 from blog.models import Post
 from django.shortcuts import get_object_or_404
-from rest_framework import status, mixins
+from rest_framework import status, mixins, viewsets
 from rest_framework.views import APIView
 from rest_framework.generics import GenericAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
@@ -120,3 +120,32 @@ class PostDetail(RetrieveUpdateDestroyAPIView):
     permission_classes = [IsAuthenticatedOrReadOnly]
     serializer_class = PostSerializer
     queryset = Post.objects.filter(status=True)
+
+
+"""--- ViewSet ---"""
+
+class PostViewSet(viewsets.ViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    serializer_class = PostSerializer
+    queryset = Post.objects.filter(status=True)
+
+    def list(self, request):
+        serializer = self.serializer_class(self.queryset, many=True)
+        return Response(serializer.data)
+    
+    def retrieve(self, request, pk=None):
+        post_object = get_object_or_404(self.queryset, pk=pk)
+        serializer = self.serializer_class(post_object)
+        return Response(serializer.data)
+
+    def create(self, request):
+        pass
+
+    def update(self, request, pk=None):
+        pass
+
+    def partial_update(self, request, pk=None):
+        pass
+
+    def destroy(self, request, pk=None):
+        pass 
