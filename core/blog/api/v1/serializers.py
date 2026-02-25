08a10128cc57bb229ae2a3d6b8f098d1c5a4522f
masoud533 <mapar10278@gmail.com>
2026-02-25 12:54:37 +0330

    add ger absolute_url field

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -8,11 +8,16 @@ from blog.models import Post, Category
 #     title = serializers.CharField(max_length=255)
 
 class PostSerializer(serializers.ModelSerializer):
+    absolute_url = serializers.SerializerMethodField()
     
     class Meta:
         model = Post
-        fields = ['id','author','title','content', 'category','created_date','status','published_date']
+        fields = ['id','author','title','content', 'category','absolute_url','created_date','status','published_date']
         # read_only_fields = ['content']
+
+    def get_absolute_url(self,obj):
+        request = self.context.get('request')
+        return request.build_absolute_uri(obj.pk)
 
 class CategorySerializer(serializers.ModelSerializer):
 
