7bac6043e46139fc9ac3f3c7bef0b3e510058da9
masoud533 <mapar10278@gmail.com>
2026-05-12 23:10:32 +0330

    modified serializers.py

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -18,6 +18,16 @@ class PostSerializer(serializers.ModelSerializer):
     def get_absolute_url(self,obj):
         request = self.context.get('request')
         return request.build_absolute_uri(obj.pk)
+    
+    def to_representation(self, instance):
+        request = self.context.get('request')
+        rep = super().to_representation(instance)
+        rep['category'] = CategorySerializer(instance.category).data
+        if request.parser_context.get('kwargs').get('pk'):
+            rep.pop('absolute_url',None)
+        else:
+            rep.pop('content',None)
+        return rep
 
 class CategorySerializer(serializers.ModelSerializer):
 
